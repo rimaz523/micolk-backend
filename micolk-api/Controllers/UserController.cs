1c212a4de3bdd3aaa7446c67b7fbf8e75722eba9
Mohommed Rimaz <rimazmohommed523@gmail.com>
2021-03-22 16:53:00 +1300

    set up clean architecture

---
+++ micolk-api/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Users.Queries.GetUserById;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace micolk_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ApiControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<UserDto>> GetUserById([FromQuery] GetUserByIdQuery query)
+        {
+            return await Mediator.Send(query);
+        }
+    }
+}
